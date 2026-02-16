b34d2f84e01580a5ff557adb80673e5cbb72b875
seojaehong <iceamericano9@gmail.com>
2026-02-16 14:13:49 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // 이 줄이 핵심입니다! 스타일을 여기서 불러옵니다.
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

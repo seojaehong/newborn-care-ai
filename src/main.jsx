ef589a75d0fdc7d0f165f68d5357b56ec07e2c72
seojaehong <iceamericano9@gmail.com>
2026-02-16 14:08:30 +0900

    Refactor React imports and rendering method
    Updated import statements for React and ReactDOM, and changed the rendering method to use React.StrictMode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'  // 이 줄이 핵심입니다! 스타일을 여기서 불러옵니다.
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
